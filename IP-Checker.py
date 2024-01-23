0011dad2534d83a39c213e13289048a20c1dced8
Chris Gallup <44879985+cgallup@users.noreply.github.com>
2024-01-23 07:42:00 -0500

    Add files via upload

---
+++ IP-Checker.py
@@ -0,0 +1,25 @@
+import subprocess
+
+def whois_lookup(ip_address):
+    result = subprocess.run(['whois', ip_address], capture_output=True, text=True)
+    print(result.stdout)
+
+def nslookup(ip_address):
+    result = subprocess.run(['nslookup', ip_address], capture_output=True, text=True)
+    print(result.stdout)
+
+def nmap_https_cert(ip_address):
+    result = subprocess.run(['nmap', '--script', 'ssl-cert', '-Pn', '443', ip_address], capture_output=True, text=True)
+    print(result.stdout)
+
+if __name__ == "__main__":
+    ip_address = input("Enter the IP address: ")
+
+    print("\nWHOIS Lookup:")
+    whois_lookup(ip_address)
+
+    print("\nNSlookup:")
+    nslookup(ip_address)
+
+    print("\nNmap HTTPS Certificate Information:")
+    nmap_https_cert(ip_address)
